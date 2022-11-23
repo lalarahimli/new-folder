41ca897d2bf76763cce21a2c2316f045150f3770
lalarahimli <114071420+lalarahimli@users.noreply.github.com>
2022-11-23 14:33:15 +0400

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Button from './components/Web4/Web'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <div>
+    <Button/>
+  </div>
+
+)
